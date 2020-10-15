98f1a823bf3ae725b119684a833ae01fcd81844a
MedJoggins <mehdi.a.escudero@gmail.com>
2020-10-15 11:18:01 +0200

    pages updated

--- src/Corner.js
+++ src/Corner.js
@@ -24,7 +24,7 @@ class Corner extends React.Component {
           dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat.{" "}
+          commodo consequat.
         </div>
       </div>
     );
