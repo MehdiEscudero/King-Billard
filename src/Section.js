ef9527a2e567ca9d5d55d8a05ec68f4505902ff5
MedJoggins <mehdi.a.escudero@gmail.com>
2020-10-21 18:59:58 +0200

    routes, link and session 50 done

--- src/Section.js
+++ src/Section.js
@@ -5,14 +5,14 @@ import { Link } from "react-router-dom";
 class Section extends React.Component {
   render() {
     return (
-      <Link style={{ color: "inherit", textDecoration: "inherit" }} to="corner">
-        {" "}
-        {/* // j'ai peut etre résolu le style check */}
-        <div className="section">
-          <img className="corner" src={this.props.element.img} alt="corner" />
-          <div className="subtitle">{this.props.element.subtitle}</div>
-          <div className="article">{this.props.element.article} </div>
-        </div>
+      <Link
+        style={{ color: "inherit", textDecoration: "inherit" }}
+        to={this.props.element.path}
+        className="section"
+      >
+        <img className="corner" src={this.props.element.img} alt="corner" />
+        <div className="subtitle">{this.props.element.subtitle}</div>
+        <div className="article">{this.props.element.article} </div>
       </Link>
     );
   }
